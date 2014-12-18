fb0570165493dad1c274635a52070f87a1e420db
Albert Weinert <info@der-albert.com>
2014-12-18 07:46:30 +0100

    horizontal or vertical form

--- src/app/core/directives/input.js
+++ src/app/core/directives/input.js
@@ -3,27 +3,67 @@
  */
 (function(angular) {
     var module = angular.module('sm.directive.input',[]);
+
+    module.directive('smForm', function() {
+        function controller($scope) {
+            this.colsLeft = $scope.colsLeft || 2;
+            this.colsRight = $scope.colsRight || 12-this.colsLeft;
+            this.horizontal = angular.isDefined($scope.horizontal) && $scope.horizontal=="true" ? true : false;
+        }
+        function link(scope, element, attrs, ctrl) {
+            console.log(ctrl);
+            if (ctrl.horizontal) {
+                element.addClass('form-horizontal');
+           }
+        }
+        controller.$inject=['$scope'];
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: true,
+            link: link,
+            controller: controller,
+            controllerAs: 'ctrl',
+            scope: {
+                colsLeft: '@',
+                colsRight: '@',
+                horizontal: '@'
+            },
+            template:
+            '<form class="form">'+
+            '<ng-transclude></ng-transclude>'+
+            '</form>'
+        }
+    });
     module.directive('smField', function() {
-        function link(scope, element, attributes){
+        function link(scope, element, attributes, ctrl){
+            console.log(ctrl);
+
             var cols = 12;
             var labelCols = 2;
-
+            if (ctrl && ctrl.colsLeft) {
+                labelCols = ctrl.colsLeft;
+            }
             if (attributes.labelCols) {
                 labelCols = parseInt(attributes.labelCols)
             }
             var inputCols = cols - labelCols;
-            if (attributes.inputCols) {
-                inputCols = parseInt(attributes.inputCols);
-                if (!attributes.labelCols) {
-                    labelCols = cols - inputCols;
-                }
+            if (ctrl && ctrl.colsRight) {
+                inputCols = ctrl.colsRight;
             }
 
+            if (attributes.inputCols) {
+                inputCols = parseInt(attributes.inputCols);
+            }
+            inputCols = Math.min(inputCols, cols-labelCols);
             var labelClass = "col-sm-" + labelCols;
             var inputClass = "col-sm-" + inputCols;
             var input = element.find('input');
             if (!input.hasClass('form-control'))
                 input.addClass('form-control');
+
+            if (ctrl && !ctrl.horizontal)
+                return;
             if (!input.hasClass(inputClass))
                 input.parent().addClass(inputClass);
             var label = element.find('label');
@@ -35,13 +75,14 @@
             restrict: 'E',
             replace: true,
             transclude: true,
+            require : '^smForm',
             scope: {
                 caption: '@'
             },
             template:
             '<div class="form-group">'+
             '<label class="control-label">{{caption}}</label>' +
-            '<div><ng-transclude></ng-transclude></div>'+
+            '<div ng-transclude></div>'+
             '</div>'
         }
     });
